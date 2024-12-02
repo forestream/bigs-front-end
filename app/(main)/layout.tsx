0d599bfe1f4de691a0dd91e30e40a2ba0f48c64b
hanbin <rj2303@naver.com>
2024-12-02 16:37:53 +0900

    유저 인증 페이지, 메인페이지 라우트 그룹 분리

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,14 @@
+import Nav from "../_view/Nav";
+
+export default function Layout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<>
+			<Nav />
+			{children}
+		</>
+	);
+}
