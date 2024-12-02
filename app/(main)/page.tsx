5058f8c43910ba13f8cbdf945b477d3b6d79e656
hanbin <rj2303@naver.com>
2024-12-02 20:04:47 +0900

    게시글 목록 불러오기 개발

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,32 @@
+"use client";
+
+import { BASE_URL } from "@/lib/constants";
+import { useEffect, useState } from "react";
+
 export default function Page() {
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		const fetchAsync = async () => {
+			try {
+				const response = await fetch(`${BASE_URL}/api/boards?page=0&size=10`);
+
+				if (!response.ok) {
+					throw new Error(response.status + response.statusText);
+				}
+
+				const body = await response.json();
+
+				setPosts(body);
+
+				console.log(body);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+
+		fetchAsync();
+	}, []);
+
 	return <main>boards</main>;
 }
--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,21 @@ import { NextRequest, NextResponse } from "next/server";
 import { MISSION_API_URL } from "./lib/constants";
 
 export async function middleware(request: NextRequest) {
-	request.headers.set("Authorization", `Bearer ${request.cookies.get("at")}`);
+	if (request.nextUrl.pathname.startsWith("/api/boards")) {
+		request.headers.set(
+			"Authorization",
+			`Bearer ${request.cookies.get("at")?.value}`
+		);
 
-	return NextResponse.rewrite(new URL(request.url, MISSION_API_URL), {
-		request: { headers: request.headers },
-	});
+		return NextResponse.rewrite(
+			new URL(request.url.split("/api")[1], MISSION_API_URL),
+			{
+				request: { headers: request.headers },
+			}
+		);
+	}
+
+	return NextResponse.next();
 }
 
 export const config = {
