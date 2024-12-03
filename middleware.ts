ccbd6d4c736083ad72b631c7ce3095ea2d4c4380
hanbin <rj2303@naver.com>
2024-12-03 18:18:07 +0900

    미들웨어로 오는 모든 요청에 리프레쉬토큰 검사 후 가능하면 재발급

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,30 @@ import { NextRequest, NextResponse } from "next/server";
 import { MISSION_API_URL } from "./lib/constants";
 
 export async function middleware(request: NextRequest) {
+	if (!request.cookies.has("at") && request.cookies.has("rt")) {
+		const response = await fetch(`${MISSION_API_URL}/auth/refresh`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				refreshToken: request.cookies.get("rt")?.value,
+			}),
+		});
+
+		const body = await response.json();
+
+		const nextResponse = new NextResponse();
+
+		nextResponse.cookies.set("at", body.accessToken, {
+			httpOnly: true,
+			maxAge: 300,
+		});
+		nextResponse.cookies.set("rt", body.refreshToken, { httpOnly: true });
+
+		return NextResponse.next({ headers: nextResponse.headers });
+	}
+
 	if (request.nextUrl.pathname.startsWith("/api/boards")) {
 		request.headers.set(
 			"Authorization",
@@ -14,32 +38,6 @@ export async function middleware(request: NextRequest) {
 				request: { headers: request.headers },
 			}
 		);
-	}
-
-	if (request.nextUrl.pathname.startsWith("/posts")) {
-		if (!request.cookies.has("at") && request.cookies.has("rt")) {
-			const response = await fetch(`${MISSION_API_URL}/auth/refresh`, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					refreshToken: request.cookies.get("rt")?.value,
-				}),
-			});
-
-			const body = await response.json();
-
-			const nextResponse = new NextResponse();
-
-			nextResponse.cookies.set("at", body.accessToken, {
-				httpOnly: true,
-				maxAge: 300,
-			});
-			nextResponse.cookies.set("rt", body.refreshToken, { httpOnly: true });
-
-			return NextResponse.next({ headers: nextResponse.headers });
-		}
 	}
 
 	return NextResponse.next();
