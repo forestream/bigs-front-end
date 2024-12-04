662c55371d21e22e7bd249067c004d0f7cf36eef
hanbin <rj2303@naver.com>
2024-12-04 18:08:28 +0900

    네브바 햄버거 메뉴 모달 UI 개발

--- app/(main)/_view/Nav.tsx
+++ app/(main)/_view/Nav.tsx
@@ -6,6 +6,9 @@ import { useRouter } from "next/navigation";
 import { authStore } from "@/stores/authStore";
 import Link from "next/link";
 import IconHamBurger from "@/components/Icons/IconHamburger";
+import { useState } from "react";
+import Modal from "@/components/Modal";
+import IconX from "@/components/Icons/IconX";
 
 export default function Nav() {
 	const router = useRouter();
@@ -17,24 +20,53 @@ export default function Nav() {
 		signOut();
 	};
 
+	const [isOpen, setIsOpen] = useState(false);
+	const handleIsOpen = (isOpen: boolean) => setIsOpen(isOpen);
+
 	return (
 		<header className={styles.nav}>
 			<Link href="/" className={styles.logo}>
 				<h1>Bigs</h1>
 			</Link>
-			<button className={styles.hamburger}>
+			<button className={styles.hamburger} onClick={() => handleIsOpen(true)}>
 				<IconHamBurger />
 			</button>
+			{isOpen && (
+				<Modal isOpen={isOpen}>
+					<Modal.Overlay>
+						<Modal.Content className={styles.modal}>
+							<button
+								className={styles.close}
+								onClick={() => handleIsOpen(false)}
+							>
+								<IconX className={styles.x} />
+							</button>
+							<h1 className={styles.logo}>Bigs</h1>
+							<div className={styles.profile}>
+								{user && (
+									<div className={styles.user}>
+										<p>{user.username}</p>
+										<p>{user.name}</p>
+									</div>
+								)}
+								<button
+									onClick={user ? handleSignout : handleSignin}
+									className={styles.button}
+								>
+									{user ? "로그아웃" : "로그인"}
+								</button>
+							</div>
+							<div className={styles.menu} onClick={() => handleIsOpen(false)}>
+								<Link href="/posts">게시판</Link>
+							</div>
+						</Modal.Content>
+					</Modal.Overlay>
+				</Modal>
+			)}
 			<div className={styles.navInner}>
 				<div className={styles.menu}>
 					<Link href="/posts">게시판</Link>
 				</div>
-				{user && (
-					<div className={styles.user}>
-						<p>{user.username}</p>
-						<p>{user.name}</p>
-					</div>
-				)}
 				<button
 					onClick={user ? handleSignout : handleSignin}
 					className={styles.button}
